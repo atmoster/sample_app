ebed3239c34689a1bed1ba1237c48650a914dc2f
Allan Moster <mosterbills@gmail.com>
2011-05-03 15:54:03 -0400

    Finished layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 SampleApp::Application.routes.draw do
   
+  get "users/new"
+
   get "pages/home"
   get "pages/contact"
   get "pages/about"
@@ -9,6 +11,8 @@ SampleApp::Application.routes.draw do
   match '/about', :to => 'pages#about'
   match '/help', :to => 'pages#help'
   root :to => 'pages#home'
+  
+  match '/signup', :to => 'users#new'
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
---
+++ spec/requests/layout_links_spec.rb
@@ -0,0 +1,30 @@
+require 'spec_helper'
+
+describe "LayoutLinks" do
+
+  it "should have a Home Page at '/'" do
+      get '/'
+      response.should have_selector('title', :content => "Home")
+  end
+  
+  it "should have a Contact Page at '/contact'" do
+      get '/contact'
+      response.should have_selector('title', :content => "Contact")
+  end
+
+  it "should have a About Page at '/about'" do
+      get '/about'
+      response.should have_selector('title', :content => "About")
+  end
+  
+  it "should have a Help Page at '/help'" do
+      get '/help'
+      response.should have_selector('title', :content => "Help")
+  end
+  
+  it "should have a Sign Up Page at '/signup'" do
+      get '/signup'
+      response.should have_selector('title', :content => "Sign Up")
+  end
+
+end
